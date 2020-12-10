33f35822376d1a19493f712f723921e1b4654505
David Mershon <dmershon@universalhappymaker.com>
2020-12-10 11:30:27 -0800

    cleaned up some formatting so the output is more readable

--- app/test_model_load.py
+++ app/test_model_load.py
@@ -2,13 +2,14 @@ from fastai.vision.all import *
 load_path = Path(__file__).parent
 classifier_save_filename = 'subculture_classifier.pkl'
 loaded_subculture_learn = load_learner(load_path/classifier_save_filename)
-print(loaded_subculture_learn.dls.vocab)
 
 def get_formatted_prediction(img_to_predict):
   prediction,p_index,probabilities = loaded_subculture_learn.predict(img_to_predict)
   return f"prediction: {prediction} probability: {probabilities[p_index]}"
 
-print('predicting james hoffmann...')
-print(get_formatted_prediction(load_path/'james_hoffmann.jpg'))
-print('predicting david and Julie...')
-print(get_formatted_prediction(load_path/'david_and_julie.jpg'))
+dj_prediction = get_formatted_prediction(load_path/'david_and_julie.jpg')
+jh_prediction = get_formatted_prediction(load_path/'james_hoffmann.jpg')
+
+print(f"\nloading categories...\n{loaded_subculture_learn.dls.vocab}")
+print(f"\npredicting james hoffmann...\n{jh_prediction}")
+print(f"\npredicting david and Julie...\n{dj_prediction}\n")
